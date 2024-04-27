6a96887117544beb7121b18f62be3d7f97fb885e
Godswill Umukoro <umukoro6@gmail.com>
2024-04-27 08:19:46 +0100

    Active link gets styled in navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,7 +1,15 @@
+'use client'
+
 import Link from "next/link"
 import { GiFly } from "react-icons/gi";
+import { usePathname } from "next/navigation";
+import classnames from "classnames";
 
 const NavBar = () => {
+    const currentPath = usePathname()
+    console.log(currentPath);
+
+
     const links = [
         {label: "Dashboard", href: "/"},
         {label: "Issues", href: "/issues"},
@@ -15,7 +23,11 @@ const NavBar = () => {
                     <Link
                         key={link.href}
                         href={link.href}
-                        className="text-zinc-500 hover:text-zinc-800 transition-colors">
+                        className={classnames({
+                            'text-zinc-900': link.href === currentPath,
+                            'text-zinc-500': link.href !== currentPath,
+                            'hover:text-zinc-800 transition-colors': true
+                        })}>
                             {link.label}
                     </Link>
                 </li>
